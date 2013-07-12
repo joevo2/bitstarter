e11dfc7c200c65ea6d8abf5bc2d54e8153b7c962
Joel Yek SE <joevo2@gmail.com>
2013-07-12 04:07:34 +0000

    modified web.js with readFileSync

--- web.js
+++ web.js
@@ -2,9 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
   response.send('Hello World from Joel Yek!');
+  fs.readFileSync(index.html);
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
