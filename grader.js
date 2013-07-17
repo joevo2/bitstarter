cebff513655c522de804ec8d3f830bc38e696566
Joel Yek SE <joevo2@gmail.com>
2013-07-17 07:31:59 +0000

    Twiiter Bottstrap update

--- grader.js
+++ grader.js
@@ -11,8 +11,8 @@ var rest = require("restler");
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -30,8 +30,8 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
@@ -40,9 +40,9 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-var checkUrl = function(url) {
-    rest.get('url').on('complete', function(data) {
-	if(data instanceof Error) {
+var check_url = function(url) {
+    rest.get(url).on('complete', function(result) {
+	if(result instanceof Error) {
 	    console.log("%s does not exists. Exiting", url);
 	    process.exit(1);
 	} else {
@@ -53,20 +53,23 @@ var checkUrl = function(url) {
     });
 };
 
-if(require.main == module) {
-    program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url>', 'Path to URL')
-	.parse(process.argv);
-    if( program.url ) {
-        check_url(program.url);
-    } else {
-        checkings(program.file);
-    }
-    var checkJson = checkHtmlFile(program.file, program.checks);
+var checkings = function(file) {
+    var checkJson = checkHtmlFile(file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+};
+
+if(require.main == module) {
+    program
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'Path to URL')
+	.parse(process.argv);
+    if(program.url) {
+	check_url(program.url);
+    } else {
+	checkings(program.file);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
